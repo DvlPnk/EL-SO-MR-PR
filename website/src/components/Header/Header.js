74b4eb96cc59d26e9751b545f8e8dde693491539
Stefano Olivieri Romero <74364933+stefa1608@users.noreply.github.com>
2021-10-20 10:20:05 -0500

    Update Header.js

--- website/src/components/Header/Header.js
+++ website/src/components/Header/Header.js
@@ -3,13 +3,14 @@ import React from 'react'
 import { useStyles } from './style'
 import { Add } from "@material-ui/icons";
 
-const Header = () => {
+const Header = ({children}) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar className={classes.appBar} position="static">
         <Toolbar className={classes.toolbar}>
           <div className={classes.headerWrapper}>
+            {children}
             <img
               src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg"
               alt="Classroom"
