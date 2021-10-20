5da4bc5d3f7c83052be02179e5fc2b25ce3760c4
Stefano Olivieri Romero <74364933+stefa1608@users.noreply.github.com>
2021-10-20 11:39:24 -0500

    Update CreateClass.js

--- website/src/components/CreateClass/CreateClass.js
+++ website/src/components/CreateClass/CreateClass.js
@@ -1,8 +1,16 @@
 import React freom "react";
+import { useContext } from "../../context/context";
 const CreateClass = () =>{
+    const { createClassDialog, setCreateClassDialog } = useContext();
   return(
+    <>  
+    {createClassDialog &&
     <div>
       <h1>Hello</h1>
     </div>
+    }}
+    </>
    );
 };
+
+export default CreateClass;
