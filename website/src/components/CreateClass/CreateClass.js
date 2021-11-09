151c798216421b71792059a52243190b2ed492f4
Stefano Olivieri Romero <74364933+stefa1608@users.noreply.github.com>
2021-11-09 13:50:24 -0500

    Update CreateClass.js

--- website/src/components/CreateClass/CreateClass.js
+++ website/src/components/CreateClass/CreateClass.js
@@ -1,6 +1,7 @@
 import { Dialog } from '@material-ui/core'
 import React from 'react'
 import { useLocalContex } from '../../context/context'
+import "./style.css";
 export const CreateClass = () => {
   const { createClassDialog, setCreateClassDialog } = useLocalContex()
   return (
@@ -12,7 +13,30 @@ export const CreateClass = () => {
             aria-labelledby="customized-dialog-title"
             open={createClassDialog}
           >
-            <h1>Creating Class</h1>
+           <div className="class__title">
+              Using Classroom at a school with students?
+            </div>
+            <DialogContent className="class__content">
+               <p classname="class__text">
+                <p>If so, your school must sign for a fee</p>
+                <a href="/help" className="class__link">
+                  G Suite for Education
+                </a>
+              account before you can use Classroom
+                <a href="/learn" className="class__link2">
+                  Learn More.
+                </a>
+              </p>
+               <p>
+                G Suite for Education lets schools decide which Google services
+                their students can use, and provides additional
+                <a href="/privacy" className="class__link2 class__link">
+                  privacy and security
+                </a>
+                protections that are important in a school setting. Students
+                cannot use Google Classroom at a school with personal accounts.
+              </p>
+             </DialogContent>
           </Dialog>
         </div >
       }
