15a8e693f60cd9dae325f3e21fc3ad5a9a7d4bb0
LP797XD <emmanuel.lapiz.p@uni.pe>
2021-11-22 15:58:34 -0500

    update

--- website/src/components/Login/Login.js
+++ website/src/components/Login/Login.js
@@ -4,12 +4,14 @@ import { useLocalContext } from "../../context/context";
 import { Button } from "@material-ui/core";
 import './style.css';
 const Login = () => {
-    const {login,loggedInUser} = useLocalContext();
+    const { login, loggedInUser } = useLocalContext();
+
+    console.log(loggedInUser);
     return (
     <div className="login">
       <img className="login__logo" src={logo} alt="Classroom" />
       <Button variant="contained" color="default" onClick={()=> login()}>
-        Ingresar Ahora!
+        Ingresa Ahora!
       </Button>
     </div>
   );
