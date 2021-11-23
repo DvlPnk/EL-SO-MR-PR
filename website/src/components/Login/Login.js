c1d1ef4b32943cf9b1bef56969e1c135024e18d8
Pierre-Ruiz <pierre.ruiz.r@uni.pe>
2021-11-22 23:31:08 -0500

    Update

--- website/src/components/Login/Login.js
+++ website/src/components/Login/Login.js
@@ -4,14 +4,14 @@ import logo from "../../assets/logo.png";
 import { useLocalContext } from "../../context/context";
 import "./style.css";
 const Login = () => {
-    const { login, loggedInUser } = useLocalContext();
+  const { login, loggedInUser } = useLocalContext();
 
-    console.log(loggedInUser);
-    return (
+  console.log(loggedInUser);
+  return (
     <div className="login">
       <img className="login__logo" src={logo} alt="Classroom" />
       <Button variant="contained" color="default" onClick={() => login()}>
-        Login Now!
+        Ingresar
       </Button>
     </div>
   );
